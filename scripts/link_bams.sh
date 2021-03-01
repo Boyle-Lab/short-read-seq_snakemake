37f35ce681b3b04a6e0df9a6afceb195dfaa827b
Adma Diehl <adadiehl@umich.edu>
2021-03-01 12:56:39 -0500

    Modified Makefile to use sampleInfo.txt as source of sample names (for bam file location, etc.). Made link_bams.sh more robust to windows-style text files.

--- scripts/link_bams.sh
+++ scripts/link_bams.sh
@@ -4,12 +4,12 @@
 # pipeline. Sample data are in SAMPLES in the root folder.
 
 SAMPLES=$1
-BAM_PATH=$2
+BAM_PATH=$2  # Use the fully-qulaified absolute path here!
 
 IFS=$'\n'
 
 # Run a loop over samples to create the links.
-for REC in $(cat $SAMPLES); do
+for REC in $(cat $SAMPLES | grep -v '^#' | tr -d '\r'); do
     read -d "\n" ID NAME <<< $(echo $REC | awk -F '\t' '{printf "%s\n%s", $3, $2}')
 
     R1=$(ls $BAM_PATH/$NAME.*.bam)
